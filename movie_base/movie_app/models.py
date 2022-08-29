9cba9bc03859c6d55770a36204f54696549cfb90
Pavel Anokhin <retyui55@gmail.com>
2022-08-29 22:01:08 +0300

    added some models. not all

---
+++ movie_base/movie_app/models.py
@@ -0,0 +1,79 @@
+from statistics import mode
+from tabnanny import verbose
+from django.db import models
+
+class Category(models.Model):
+    '''Категории'''
+    name = models.CharField('Категория', max_length=150)
+    description = models.TextField('Описание')
+    url = models.SlugField(max_length=150)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+class Actor(models.Model):
+    '''Актёры и режиссёры'''
+    name = models.CharField('Имя', max_length=100)
+    age = models.PositiveSmallIntegerField('Возраст', default=0)
+    description = models.TextField('Описание')
+    image = models.ImageField('Изображение', upload_to='actors/')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Актёр или режиссер'
+        verbose_name_plural = 'Актёры и режиссеры'
+
+        
+class Genre(models.Model):
+    '''Жанры'''
+    name = models.CharField('Название жанра', max_length=150)
+    description = models.TextField('Описание')
+    url = models.SlugField(max_length=150)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+class Movie(models.Model):
+    '''Фильмы'''
+    title = models.CharField('Название', max_length=100)
+    tagline = models.CharField('Слоган', max_length=300)
+    description = models.TextField('Описание')
+    poster = models.ImageField('Постер', upload_to='movies/')
+    year = models.PositiveSmallIntegerField('Дата выхода', default=2019)
+    country = models.CharField('Страна', max_length=50)
+    directors = models.ManyToManyField(Actor, verbose_name='Режиссер', related_name='film_director')
+    actors = models.ManyToManyField(Actor, verbose_name='Актёр', related_name='film_actor')
+    genres = models.ManyToManyField(Genre, verbose_name='Жанры')
+    world_premiere = models.DateField('Премьера в мире', default=date.today)
+    budget = models.PositiveIntegerField('Бюджет', default=0, help_text='указывать сумму в долларах')
+    fees_in_usa = models.PositiveIntegerField(
+        'Сборы в США', default=0, hel_text='указывать сумму в долларах'
+        )
+    fees_in_world = models.PositiveIntegerField(
+        'Сборы в мире', default=0, hel_text='указывать сумму в долларах'
+        )
+    category = models.ForeignKey(
+        Category, verbose_name='Категория', on_delete=models.SET_NULL, null=True
+        )
+    url = models.SlugField(max_length=160, unique=True)
+    draft = models.BooleanField('Черновик', default=False)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Фильм'
+        verbose_name_plural = 'Фильмы'
+
+class MovieShot(models.Model):
+    '''Кадры из фильма'''
