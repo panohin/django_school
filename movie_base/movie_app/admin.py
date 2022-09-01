48464227201035a66ebf172a2bcea8e62a766895
Pavel <retyui55@gmail.com>
2022-09-01 16:50:11 +0300

    work with admin panel

--- movie_base/movie_app/admin.py
+++ movie_base/movie_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+admin.site.register(models.Category)
+admin.site.register(models.Actor)
+admin.site.register(models.Genre)
+admin.site.register(models.RatingStar)
+admin.site.register(models.Rating)
+admin.site.register(models.Review)
--- movie_base/movie_app/models.py
+++ movie_base/movie_app/models.py
@@ -1,6 +1,7 @@
 from statistics import mode
 from tabnanny import verbose
 from django.db import models
+from datetime import date
 
 class Category(models.Model):
     '''Категории'''
@@ -57,10 +58,10 @@ class Movie(models.Model):
     world_premiere = models.DateField('Премьера в мире', default=date.today)
     budget = models.PositiveIntegerField('Бюджет', default=0, help_text='указывать сумму в долларах')
     fees_in_usa = models.PositiveIntegerField(
-        'Сборы в США', default=0, hel_text='указывать сумму в долларах'
+        'Сборы в США', default=0, help_text='указывать сумму в долларах'
         )
     fees_in_world = models.PositiveIntegerField(
-        'Сборы в мире', default=0, hel_text='указывать сумму в долларах'
+        'Сборы в мире', default=0, help_text='указывать сумму в долларах'
         )
     category = models.ForeignKey(
         Category, verbose_name='Категория', on_delete=models.SET_NULL, null=True
@@ -77,3 +78,57 @@ class Movie(models.Model):
 
 class MovieShot(models.Model):
     '''Кадры из фильма'''
+    title = models.CharField('Заголовок', max_length=100)
+    description = models.TextField('Описание')
+    image = models.ImageField('Изображение', upload_to='movie_shots/')
+    movie = models.ForeignKey(Movie, verbose_name='Фильм', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Кадры из фильма'
+        verbose_name = 'Кадр из фильма'
+
+class RatingStar(models.Model):
+    '''Звезда рейтинга'''
+    value = models.PositiveSmallIntegerField('Значение', default=0)
+
+    def __str__(self):
+        return self.value
+
+    class Meta:
+        verbose_name = 'Звезда рейтинга'
+        verbose_name_plural = 'Звёзды рейтинга'
+
+class Rating(models.Model):
+    '''Рейтинг'''
+    ip = models.CharField('IP', max_length=15)
+    star = models.ForeignKey(
+        RatingStar, verbose_name='Количество звёзд', on_delete=models.CASCADE
+        )
+    movie = models.ForeignKey(Movie, verbose_name='Фильм', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.star} - {self.movie}'
+
+    class Meta:
+        verbose_name = 'Рейтинг'
+        verbose_name_plural = 'Рейтинги'
+
+class Review(models.Model):
+    '''Отзывы к фильму'''
+    movie = models.ForeignKey(Movie, verbose_name='Фильм', on_delete=models.CASCADE)
+    text = models.TextField('Отзыв')
+    email = models.EmailField()
+    name = models.CharField('Имя автора отзыва', max_length=70)
+    parent = models.ForeignKey(
+        'self', verbose_name='Родитель', on_delete=models.SET_NULL, null=True, blank=True
+        )
+
+    def __str__(self):
+        return f'{self.movie} - {self.name}'
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
