2903713c5ace1cbafddffdc783b0d9c6d243c974
Pavel <retyui55@gmail.com>
2022-09-10 06:10:26 +0300

    added class-based views

--- movie_base/movie_app/urls.py
+++ movie_base/movie_app/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import MoviesView, MovieDetailView
+from .views import MoviesView, MovieDetailView, UserTemplateView
 
 
 urlpatterns = [
+    path('info', UserTemplateView.as_view()),
+
     path('<slug:slug>', MovieDetailView.as_view(), name='movie_detail_url'),
     path('', MoviesView.as_view(),  name='movie_list_url'),
+    
 ]
--- movie_base/movie_app/views.py
+++ movie_base/movie_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic.base import View
+from django.views.generic.base import View, TemplateView
 
 from .models import Movie
 
@@ -14,3 +14,6 @@ class MovieDetailView(View):
 	def get(self, request, slug):
 		movie = get_object_or_404(Movie, url=slug)
 		return render(request, 'movie_app/movie_detail.html', {'movie':movie})
+
+class UserTemplateView(TemplateView):
+	template_name = 'movie_app/info.html'
